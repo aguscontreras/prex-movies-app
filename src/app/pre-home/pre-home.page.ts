8b644491a679a9d7647f1de0745b0c091ad27e56
Agustin Contreras <aguscontreras@outlook.com.ar>
2023-10-08 13:22:16 -0300

    add pre home page

--- src/app/pre-home/pre-home.page.ts
+++ src/app/pre-home/pre-home.page.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { LogoComponent } from '../shared/logo/logo.component';
 
 @Component({
   selector: 'app-pre-home',
   templateUrl: './pre-home.page.html',
   styleUrls: ['./pre-home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    LogoComponent,
+  ],
 })
-export class PreHomePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PreHomePage {
+  constructor() {}
 }
