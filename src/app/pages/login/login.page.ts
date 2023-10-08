94c007e4010bb808b9af1d2218e51c53c5a9e6cb
Agustin Contreras <aguscontreras@outlook.com.ar>
2023-10-08 17:44:14 -0300

    add login page implementation

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { LogoComponent, PasswordTogglerComponent } from '../../shared';
+import { AuthService, ToastService } from '../../services';
+import { Router, RouterModule } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [
+    IonicModule,
+    CommonModule,
+    ReactiveFormsModule,
+    LogoComponent,
+    PasswordTogglerComponent,
+    RouterModule,
+  ],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  form = this.initForm();
 
-  constructor() { }
+  private readonly destroyRef = inject(DestroyRef);
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastService: ToastService,
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  initForm() {
+    return this.formBuilder.nonNullable.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
   }
 
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+
+      this.toastService.showDanger({
+        message: 'Please review the entered data',
+      });
+
+      throw new Error('[Sign Up] Invalid form.');
+    }
+
+    const { email, password } = this.form.getRawValue();
+
+    this.authService
+      .signIn(email, password)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: () => this.router.navigate(['/home']),
+      });
+  }
+
+  get email() {
+    return this.form.controls.email;
+  }
+
+  get password() {
+    return this.form.controls.password;
+  }
 }
