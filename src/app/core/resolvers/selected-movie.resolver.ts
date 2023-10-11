b131aac5c5a407278789f21b18f0738315f8bf48
Agustin Contreras <aguscontreras@outlook.com.ar>
2023-10-11 13:52:03 -0300

    clean get movie resolver

--- src/app/core/resolvers/selected-movie.resolver.ts
+++ src/app/core/resolvers/selected-movie.resolver.ts
@@ -1,23 +1,11 @@
-import {
-  ActivatedRouteSnapshot,
-  ResolveFn,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn } from '@angular/router';
 import { Movie } from '../../models';
 import { inject } from '@angular/core';
 import { MoviesService } from '../../services';
-import { of } from 'rxjs';
 
-export const selectedMovieResolver: ResolveFn<Movie | undefined> = (
+export const selectedMovieResolver: ResolveFn<Movie | {}> = (
   route: ActivatedRouteSnapshot
 ) => {
   const id = Number(route.paramMap.get('id'));
-
-  console.log(id);
-
-  if (!isNaN(id)) {
-    return inject(MoviesService).getMovie(id);
-  }
-
-  return of(undefined);
+  return inject(MoviesService).getMovie(id);
 };
