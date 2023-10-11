f81473c70da0d4602f3e2c00d20db53ac331a50a
Agustin Contreras <aguscontreras@outlook.com.ar>
2023-10-11 13:51:47 -0300

    get genres on load

--- src/app/core/resolvers/movies.resolver.ts
+++ src/app/core/resolvers/movies.resolver.ts
@@ -2,7 +2,15 @@ import { ResolveFn } from '@angular/router';
 import { Movie } from '../../models';
 import { inject } from '@angular/core';
 import { MoviesService } from '../../services';
+import { forkJoin } from 'rxjs';
 
-export const moviesResolver: ResolveFn<Movie[]> = () => {
-  return inject(MoviesService).getMovies();
+export const moviesResolver: ResolveFn<{
+  movies: Movie[];
+  genres: string[];
+}> = () => {
+  const moviesService = inject(MoviesService);
+  return forkJoin({
+    movies: moviesService.getMovies(),
+    genres: moviesService.getGenres(),
+  });
 };
