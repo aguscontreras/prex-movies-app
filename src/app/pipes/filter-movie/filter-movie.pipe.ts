944cbc9452f9e57083ed575be739447aefff97a5
Agustin Contreras <aguscontreras@outlook.com.ar>
2023-10-13 19:23:38 -0300

    update filters pipe

--- src/app/pipes/filter-movie/filter-movie.pipe.ts
+++ src/app/pipes/filter-movie/filter-movie.pipe.ts
@@ -1,18 +1,33 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Movie } from '../../models';
+import { AdvancedFilter } from '../../services';
 
 @Pipe({
   name: 'filterMovie',
   standalone: true,
 })
 export class FilterMoviePipe implements PipeTransform {
-  transform(movies: Movie[] | null, searchQuery: string): Movie[] {
-    if (movies?.length) {
-      return movies?.filter(({ title }) =>
-        title.toUpperCase().includes(searchQuery.toUpperCase())
-      );
+  transform(
+    movies: Movie[] | null,
+    movieTitle: string,
+    advancedFilters?: AdvancedFilter | null
+  ): Movie[] {
+    if (!movies) return [];
+
+    if (advancedFilters) {
+      const { rating, genres } = advancedFilters;
+
+      if (rating) {
+        movies = movies.filter((m) => m.rating <= rating);
+      }
+
+      if (genres && genres.length) {
+        movies = movies.filter((m) => m.genres.some((e) => genres.includes(e)));
+      }
     }
 
-    return [];
+    return movies.filter(({ title }) =>
+      title.toUpperCase().includes(movieTitle.toUpperCase())
+    );
   }
 }
