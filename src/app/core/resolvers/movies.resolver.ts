cd9557481535ba8e82184ea06adc8bb8d3459910
Agustin Contreras <aguscontreras@outlook.com.ar>
2023-10-14 13:43:52 -0300

    Fix movies resolver
    Now movies resolver doesnt fetch movies if we come from the Details page
    Instead it returns the local stored movies

--- src/app/core/resolvers/movies.resolver.ts
+++ src/app/core/resolvers/movies.resolver.ts
@@ -1,16 +1,24 @@
-import { ResolveFn } from '@angular/router';
+import { ResolveFn, Router } from '@angular/router';
 import { Movie } from '../../models';
 import { inject } from '@angular/core';
 import { MoviesService } from '../../services';
-import { forkJoin } from 'rxjs';
+import { combineLatest, first } from 'rxjs';
 
 export const moviesResolver: ResolveFn<{
   movies: Movie[];
   genres: string[];
 }> = () => {
+  const router = inject(Router);
+
+  const [, lastRoute] = router.routerState.snapshot.url.split('/');
+
   const moviesService = inject(MoviesService);
-  return forkJoin({
-    movies: moviesService.getMovies(),
+
+  return combineLatest({
+    movies:
+      lastRoute === 'details'
+        ? moviesService.movies$
+        : moviesService.getMovies(),
     genres: moviesService.getGenres(),
-  });
+  }).pipe(first());
 };
